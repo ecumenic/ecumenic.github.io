e55f9b4ae1f15755c1570620b9cb881d13ae9f13
Felipe Ribeiro <46685263+operarioribeiro@users.noreply.github.com>
2019-09-03 23:50:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/* PRELOADER */
+$(window).on('load', function () {
+$('#loading .img').fadeOut();
+$('#loading').delay(150).fadeOut('slow'); 
+$('body').delay(150).css({'overflow': 'visible'});
+})
+
+/* CREDITS */
+/* https://stackoverflow.com */
+/* https://codepen.com */
